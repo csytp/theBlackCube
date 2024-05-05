904b3e7f0317385546b5591b4fbf4eaea4b1bd66
Massimo Reina <99958928+stomx74@users.noreply.github.com>
2024-05-05 21:22:06 +0200

    test commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import 'style.css';
+import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { BoxLineGeometry } from 'three/examples/jsm/Addons.js';
@@ -55,16 +55,16 @@ const options = {
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xffffff );
 
-// Camera
-const camera = new THREE.PerspectiveCamera(50, options.width / options.height, 0.1, 1000);
-camera.position.z = 10;
-camera.position.y = 0;
-
 // Render
 const renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(options.width, options.height);
 renderer.setPixelRatio(2);
 document.body.appendChild(renderer.domElement);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(50, options.width / options.height, 1, 1000 );
+camera.position.z = -10;
+camera.position.y = 2;
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -75,7 +75,7 @@ controls.dampingFactor = 0.05;
 // ROOM
 
 const room = new THREE.LineSegments(
-  new BoxLineGeometry( 60, 60, 60, 100, 100, 100 ).translate( 0, 3, 0 ),
+  new BoxLineGeometry( 20, 10, 25, 100, 100, 100 ).translate( 0, 0, 0 ),
   new THREE.LineBasicMaterial( { color: 0x808080 } )
 );
 scene.add( room );
@@ -84,6 +84,7 @@ scene.add( room );
 const masterCubeGeo = new THREE.BoxGeometry(3, 3, 3);
 const masterCubeMat = new THREE.MeshLambertMaterial({color: 0xffffff, transparent: true, opacity: 0.0})
 const masterCubeMesh = new THREE.Mesh(masterCubeGeo, masterCubeMat);
+masterCubeMesh.position.y = 0;
 
 function createCube(color){
   const meshGeometry = new THREE.BoxGeometry(1, 1, 1);
@@ -165,6 +166,12 @@ function animate(){
   requestAnimationFrame( animate );
   
   controls.update();
+  renderer.render(scene, camera);
+
+  masterCubeMesh.rotation.x +=0.01;
+  masterCubeMesh.rotation.y +=0.02;
+  masterCubeMesh.rotation.z +=0.008;
+
   /*
   cube2.mesh.rotation.x += 0.01;
   cube3.mesh.rotation.x += 0.01;
@@ -179,6 +186,6 @@ function animate(){
     cube.material.color.set(getRandomColor());
   
   });*/
-  renderer.render(scene, camera);
+  
 }
 animate();
