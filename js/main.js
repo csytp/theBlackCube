8470bbf58527b430727134af0a290a958eff9349
Massimo Reina <99958928+stomx74@users.noreply.github.com>
2024-05-16 10:09:49 +0200

    - cleaning index.html - optimizing css - (main.js) remove OrbitControls - (main.js) cleaning code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import './../css/style.css';
 import * as THREE from 'three';
-import { OrbitControls, BoxLineGeometry, FontLoader, TTFLoader, TextGeometry,TrackballControls} from 'three/examples/jsm/Addons.js';
+import { BoxLineGeometry, FontLoader, TTFLoader, TextGeometry,TrackballControls} from 'three/examples/jsm/Addons.js';
 import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/Addons.js';
 import { LightProbeGenerator  } from 'three/examples/jsm/Addons.js';
 //import { GUI } from 'three/examples/jsm/Addons.js';
@@ -109,38 +109,8 @@ const planeSideWallLeft = new THREE.Mesh( new THREE.PlaneGeometry( 25, 10 ), mat
 const planeSideWallRight = new THREE.Mesh( new THREE.PlaneGeometry( 25, 10 ), material );
 
 const planeFloor = new THREE.Mesh( new THREE.PlaneGeometry( 20, 25 ), material );
-//const planeRoof = new THREE.Mesh( new THREE.PlaneGeometry( 20, 10 ), material );
 const planeRoof = new THREE.Mesh( new THREE.PlaneGeometry( 20, 25 ), material );
 
-// Lights from panels
-/*
-const lightPanel1 = new THREE.PointLight( 0xFFFFFF, 100, 100);
-lightPanel1.position.set(0, 2, -3);
-camera.add( lightPanel1 );
-*/
-/*
-// left light
-const lightPanel2 = new THREE.PointLight( 0xFFFFFF, 50, 500);
-lightPanel2.position.set(-7, 2, -10);
-camera.add( lightPanel2 );
-// right light
-
-const lightPanel3 = new THREE.PointLight( 0xFFFFFF, 50, 500);
-lightPanel3.position.set(7, 2, -10);
-camera.add( lightPanel3 );
-*/
-//front direct light
-/*
-const lightPanel4 = new THREE.DirectionalLight( 0xFF0000, 10);
-lightPanel4.position.set(0, 0, -1);
-camera.add( lightPanel4 );*/
-//front up direct light
-
-
-/*
-const pointLightHelper = new THREE.DirectionalLightHelper( lightPanel5 );
-camera.add( pointLightHelper );
-*/
 planeFrontWall.position.z = -12.4;
 planeBackWall.position.z = 2;
 
@@ -171,48 +141,40 @@ room.add( planeRoof );
 
 // FIX ROOM (and rotate only the mastercube)
 scene.add( camera ); // required when the camera has a child
-//scene.add( camera2 );
 camera.add( room );
 //scene.add(room); // when testing room structure
 
-const spotLight1 = new THREE.SpotLight( 0xFFFFFF, 250);
-spotLight1.position.set( 2, 3, -8 );
-spotLight1.angle = Math.PI / 4; // apertura del cono luce
+const spotLight1 = new THREE.SpotLight( 0xFFFFFF, 150);
+spotLight1.position.set( 4, 3, -5 );
+spotLight1.angle = Math.PI / 1.5; // apertura del cono luce
 spotLight1.penumbra = 0.2;
 spotLight1.decay = 2;
 spotLight1.distance = 0;
 
-const spotLight2 = new THREE.SpotLight( 0xFFFFFF, 250);
-spotLight2.position.set( -2, 3, -8 );
-spotLight2.angle = Math.PI / 4; // apertura del cono luce
+const spotLight2 = new THREE.SpotLight( 0xFFFFFF, 150);
+spotLight2.position.set( -4, 3, -5 );
+spotLight2.angle = Math.PI / 1.5; // apertura del cono luce
 spotLight2.penumbra = 0.2;
 spotLight2.decay = 2;
 spotLight2.distance = 0;
 
-const spotLight3 = new THREE.SpotLight( 0xFFFFFF, 250);
-spotLight3.position.set( 2, -3, -8 );
-spotLight3.angle = Math.PI / 4; // apertura del cono luce
+const spotLight3 = new THREE.SpotLight( 0xFFFFFF, 150);
+spotLight3.position.set( 4, -3, -5 );
+spotLight3.angle = Math.PI / 1.5; // apertura del cono luce
 spotLight3.penumbra = 0.2;
 spotLight3.decay = 2;
 spotLight3.distance = 0;
 
-const spotLight4 = new THREE.SpotLight( 0xFFFFFF, 250);
-spotLight4.position.set( -2, -3, -8 );
-spotLight4.angle = Math.PI / 4; // apertura del cono luce
+const spotLight4 = new THREE.SpotLight( 0xFFFFFF, 150);
+spotLight4.position.set( -4, -3, -5 );
+spotLight4.angle = Math.PI / 1.5; // apertura del cono luce
 spotLight4.penumbra = 0.2;
 spotLight4.decay = 2;
 spotLight4.distance = 0;
 
-const lightPanel3 = new THREE.PointLight( 0xFFFFFF, 50, 500);
-lightPanel3.position.set(0,0,0);
-camera.add( lightPanel3 );
-/*
-spotLight.castShadow = true;
-spotLight.shadow.mapSize.width = 512;
-spotLight.shadow.mapSize.height = 512;
-spotLight.shadow.camera.near = 1;
-spotLight.shadow.camera.far = 10;
-spotLight.shadow.focus = 1;*/
+const lightPanel3 = new THREE.PointLight( 0xFFFFFF, 100, 250);
+lightPanel3.position.set(0,0,-3);
+
 camera.add( spotLight1 );
 camera.add( spotLight2 );
 camera.add( spotLight3 );
@@ -308,7 +270,6 @@ masterCubeMesh.children.forEach(function(cube){
   console.log(cube);
 
 });*/
-
 // Lights
 /*
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
@@ -331,24 +292,13 @@ camera.add( light );
 */
 // Controls
 const controls = new TrackballControls(camera, renderer.domElement);
-//controls.enableDamping = true;
-//controls.dampingFactor = 0.5;
-//controls.target = new THREE.Vector3( 0, 1, 1.8 );
 
-//controls.minDistance = 0;
 controls.noPan = true;
 controls.maxDistance = controls.minDistance = 0;  
 controls.noKeys = true;
 controls.noRotate = false;
 controls.noZoom = true;
-//controls.maxDistance = 1000;
 
-/*
-controls.minPolarAngle = 0; // radians
-controls.maxPolarAngle = Math.PI; // radians
-controls.minAzimuthAngle = - Infinity; // radians
-controls.maxAzimuthAngle = Infinity;
-*/
 controls.addEventListener('change', () => {
   masterCubeMesh.position.copy(controls.target.clone());
 });
@@ -384,7 +334,11 @@ window.addEventListener('pointermove', (e) => {
   });*/
 
 });
-
+masterCubeMesh.children.forEach(function(cube){
+  console.log(cube.obj);
+  //cube.mesh.rotation.y = t;
+  
+});
 // Loop
 var t = 0;
 function animate(){
@@ -392,19 +346,17 @@ function animate(){
   controls.update();
   renderer.render(scene, camera);
 
-  //spotLight.position.set(1.5, -2, Math.sin(t)+2);
   t += 0.05;
-/*
+
   masterCubeMesh.rotation.x += 0.008;
   masterCubeMesh.rotation.z += 0.008;
   masterCubeMesh.rotation.z += 0.008;
-*/
 }
 animate();
 
 function onWindowResize() {
-/*
+
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );*/
+	renderer.setSize( window.innerWidth, window.innerHeight );
 }
