0d0859da0c48cf671766cff1f3274f4c49e39573
Giuseppe Ernandez <giuseppeernandez@hotmail.it>
2024-09-13 18:50:19 +0200

    added socket face recognition

--- js/main.js
+++ js/main.js
@@ -71,6 +71,11 @@ socket.on("chgScn", (ctrlValue) => {
   sketch.changeScene(ctrlValue);
 });
 
+
+socket.on("showFr", (ctrlValue) => {
+  sketch.showFr(ctrlValue);
+});
+
 // SOCKET-IO -> WEB AUDIO
 
 socket.on("csound", (ctrlValue) => {
